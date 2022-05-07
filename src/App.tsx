06fb1e71e6829093f6d5d3209d76688331962a6c
0xburden <0xburden@protonmail.com>
2022-05-07 05:39:41 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Modal from "./Modal";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-mono">The modal will pop up above this</h1>
+
+      <div className="mt-8">
+        <Modal triggerText="modal trigger method 1">
+          <h2 className="text-center text-xl font-bold">
+            Wow. I'm in this MF.
+          </h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Modal>
+      </div>
+
+      <div className="mt-8">
+        <Modal
+          trigger={
+            <button
+              type="button"
+              className="bg-red-500 text-white font-bold px-4 py-2 rounded-md shadow-lg"
+            >
+              Bop It
+            </button>
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <h2 className="text-center text-xl font-bold">
+            Wow. I'm in this MF.
+          </h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Modal>
+      </div>
+
+      <div className="mt-8">
+        <Modal
+          trigger={
+            <div className="bg-green-500 cursor-pointer text-white font-bold px-4 py-2 rounded-md shadow-lg">
+              Twist it
+            </div>
+          }
+        >
+          <h2 className="text-center text-xl font-bold">
+            Wow. I'm in this MF.
+          </h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </Modal>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Modal.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { PortalWithState } from "react-portal";
+
+type Props = {
+  trigger?: React.ReactNode;
+  triggerText?: string;
+  onClose?: () => void;
+  onOpen?: () => void;
+  children: React.ReactNode;
+};
+
+export default function Modal({
+  children,
+  trigger,
+  triggerText,
+  onClose,
+  onOpen,
+}: Props) {
+  return (
+    <PortalWithState
+      node={document.querySelector("body")}
+      closeOnOutsideClick
+      closeOnEsc
+    >
+      {({ openPortal, closePortal, isOpen, portal }) => (
+        <>
+          {trigger ? (
+            React.cloneElement(trigger as React.ReactElement, {
+              onClick: openPortal,
+            })
+          ) : (
+            <button type="button" onClick={openPortal}>
+              {triggerText}
+            </button>
+          )}
+          {portal(
+            <div
+              role="dialog"
+              aria-modal={true}
+              className="fixed inset-0 backdrop-blur flex justify-center"
+            >
+              <div className="flex flex-col justify-between mt-12 p-4 bg-white rounded-xl shadow-xl border w-full max-w-2xl min-h-[200px] max-h-[500px] overflow-y-auto">
+                <div className="h-full">{children}</div>
+                <div className="flex items-center justify-center mt-8">
+                  <button
+                    type="button"
+                    onClick={closePortal}
+                    className="border w-full bg-blue-500 text-white font-bold text-center px-4 py-2 rounded-xl"
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+    </PortalWithState>
+  );
+}
